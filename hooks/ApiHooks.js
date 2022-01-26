a5c5f66290dee73088fe8d4fa5d493bdce27c7c1
Dieu Vu <dieu.vu@metropolia.fi>
2022-01-26 10:17:53 +0200

    get userprofile picture in profile page

--- hooks/ApiHooks.js
+++ hooks/ApiHooks.js
@@ -85,4 +85,17 @@ const useUser = () => {
   return {getUserByToken, postUser};
 };
 
-export {useMedia, useLogin, useUser};
+const useTag = () => {
+  const postTag = async (data) => {
+    const options = {
+      method: 'POST',
+    };
+    return await doFetch(baseUrl, options);
+  };
+
+  const getFilesByTag = async (tag) => {
+    return await doFetch(`${baseUrl}tags/${tag}`);
+  };
+  return {postTag, getFilesByTag};
+};
+export {useMedia, useLogin, useUser, useTag};
--- views/Profile.js
+++ views/Profile.js
@@ -1,16 +1,42 @@
-import React, {useContext} from 'react';
-import {StyleSheet, SafeAreaView, Text, Button} from 'react-native';
+import React, {useContext, useEffect, useState} from 'react';
+import {StyleSheet, SafeAreaView, Text, Button, Image} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {MainContext} from '../contexts/MainContext';
+import {useTag} from '../hooks/ApiHooks';
+import {uploadsUrl} from '../utils/variables';
 
 const Profile = () => {
   const {setIsLoggedIn, user} = useContext(MainContext);
+  const [avatar, setAvatar] = useState('http://placekitten.com/640/640');
+  const {getFilesByTag} = useTag();
+
   console.log('Profile', user);
+
+  const fetchAvatar = async () => {
+    try {
+      const avatarArray = await getFilesByTag('avatar_' + user.user_id);
+      const avatar = avatarArray.pop();
+      setAvatar(uploadsUrl + avatar.filename);
+    } catch (e) {
+      console.error(e.message);
+      // in production should return some alerts to the user
+      throw new Error('Issue when loading profile picture');
+    }
+  };
+
+  useEffect(() => {
+    fetchAvatar();
+  });
 
   return (
     <SafeAreaView style={styles.container}>
       <Text>Profile</Text>
       <Text>{user.username}</Text>
+      <Image
+        source={{uri: avatar}}
+        style={{width: '80%', height: '50%'}}
+        resizeMode="contain"
+      ></Image>
       <Text>{user.email}</Text>
       <Button
         title="Log out!"
