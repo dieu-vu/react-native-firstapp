9f17fa00556560edfbdaeef356c41731270117dd
Dieu Vu <dieu.vu@metropolia.fi>
2022-01-29 10:55:20 +0200

    user react native elements for home view

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,58 +1,40 @@
 import React from 'react';
-import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
+import {Avatar, Button, ListItem as NBListItem} from 'react-native-elements';
 import PropTypes from 'prop-types';
 import {uploadsUrl} from '../utils/variables';
 
 const ListItem = ({navigation, singleMedia}) => {
   return (
-    <TouchableOpacity
-      style={styles.row}
-      onPress={() => {
-        navigation.navigate('Single', {file: singleMedia});
-      }}
-    >
-      <View style={styles.imagebox}>
-        <Image
-          style={styles.itemImg}
-          source={{
-            uri: uploadsUrl + singleMedia.thumbnails.w160,
-          }}
-        />
-      </View>
-      <View style={styles.textbox}>
-        <Text style={styles.listTitle}>{singleMedia.title}</Text>
-        <Text>{singleMedia.description}</Text>
-      </View>
-    </TouchableOpacity>
+    <NBListItem bottomDivider>
+      <Avatar
+        size="large"
+        source={{
+          uri: uploadsUrl + singleMedia.thumbnails.w160,
+        }}
+      />
+      <NBListItem.Content>
+        <NBListItem.Title numberOfLines={1} h4>
+          {singleMedia.title}
+        </NBListItem.Title>
+        <NBListItem.Subtitle>{singleMedia.description}</NBListItem.Subtitle>
+      </NBListItem.Content>
+      <Button
+        title="View"
+        buttonStyle={{
+          backgroundColor: 'rgba(78, 116, 289, 1)',
+          borderRadius: 3,
+        }}
+        containerStyle={{
+          width: '20%',
+          marginVertical: 10,
+        }}
+        onPress={() => {
+          navigation.navigate('Single', {file: singleMedia});
+        }}
+      />
+    </NBListItem>
   );
 };
-
-const styles = StyleSheet.create({
-  row: {
-    flex: 1,
-    flexDirection: 'row',
-    padding: 10,
-    backgroundColor: '#eee',
-    borderRadius: 6,
-    margin: 5,
-  },
-  textbox: {
-    flex: 2,
-    padding: 10,
-  },
-  imagebox: {
-    flex: 1,
-  },
-  itemImg: {
-    flex: 1,
-    borderRadius: 6,
-  },
-  listTitle: {
-    fontWeight: 'bold',
-    fontSize: 20,
-    paddingBottom: 15,
-  },
-});
 
 ListItem.propTypes = {
   singleMedia: PropTypes.object.isRequired,
